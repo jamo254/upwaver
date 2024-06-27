eb139fc07dbabad3a44de2d753cbdb75f26ff21b
Fred Denis <fredjamo68@gmail.com>
2024-06-28 02:02:26 +0300

    Creating sign up page

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+          # Creating validation for the user
+  validates :full_name, presence: true, length: { maximum: 50 }
+
+end
---
+++ db/migrate/20240627220229_add_fullname_to_users.rb
@@ -0,0 +1,9 @@
+class Addfull_nameToUsers < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :full_name, :string
+    add_column :users, :from, :string
+    add_column :users, :about, :text
+    add_column :users, :language, :string
+    add_column :users, :status, :boolean, default: false
+  end
+end
