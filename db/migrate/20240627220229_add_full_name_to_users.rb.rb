aaf7060ed2656f6b03c12e98f7d95a3681eca047
Fred Denis <fredjamo68@gmail.com>
2024-07-03 14:40:47 +0300

    edit migration file

--- db/migrate/20240627220229_add_full_name_to_users.rb.rb
+++ db/migrate/20240627220229_add_full_name_to_users.rb.rb
@@ -1,4 +1,4 @@
-class Addfull_nameToUsers < ActiveRecord::Migration[7.1]
+class AddFullNameToUsers < ActiveRecord::Migration[7.1]
   def change
     add_column :users, :full_name, :string
     add_column :users, :from, :string
